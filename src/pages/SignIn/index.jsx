2e85b0e430af2fcb01169b213f5a51687dddf395
JrSantiago <jrsantiaggo@gmail.com>
2023-03-26 02:54:56 -0300

    adicionando conexão com a api

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,8 +3,25 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import {  FiMail, FiLock} from 'react-icons/fi';
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function SignIn(){
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useAuth();
+
+  function handleSignIn(){
+
+    if(!email || !password){
+      return alert("Preencha todos os campos!")
+    }
+
+    signIn({email, password});
+    
+  }
+
   return(
     <Container>
       <Form>
@@ -16,15 +33,17 @@ export function SignIn(){
           placeholder="E-mail"
           type="email"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">
           Criar Conta
